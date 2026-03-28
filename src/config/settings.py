24d042210cf5b32249dd0a347a3af5f76550a32a
matiasviola <matiasviola02@gmail.com>
2026-03-28 20:08:11 -0300

    Fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 # Configuración simple
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 DB_URI = os.getenv("DATABASE_URL")  # La de postgresql:// para vectores
 BUCKET_LOGOS = "TUMARK"       # El nombre de tu bucket en Supabase
 
@@ -22,5 +22,5 @@ DIR_TEMPORAL = os.path.join(BASE_DIR, "..", "temp_downloads")
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION", "us-east-2")
+AWS_REGION = os.getenv("AWS_REGION")
 SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL")
