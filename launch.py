0877c7ad5507ac577442363fb979c64e4fade8b7
matiasviola <matiasviola02@gmail.com>
2026-03-29 01:06:17 -0300

    Fix

--- launch.py
+++ launch.py
@@ -23,7 +23,7 @@ import boto3
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURACIÓN AWS
